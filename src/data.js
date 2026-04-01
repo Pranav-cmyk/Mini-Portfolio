b60d849eda5fc50c47fccc46e6e7e79387f91125
pranav <pranavsagar530@gmail.com>
2026-04-01 10:52:44 +0530

    Trying new path

--- src/data.js
+++ src/data.js
@@ -4,20 +4,20 @@ export const blogPosts = [
     description:
       "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
     date: "JULY 23, 2022",
-    image: "./src/assets/article-image-03.png",
+    image: "src/assets/article-image-03.png",
   },
   {
     title: "Blog Two",
     description:
       "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
     date: "JULY 23, 2022",
-    image: "./src/assets/article-image-02.png",
+    image: "src/assets/article-image-02.png",
   },
   {
     title: "Blog Three",
     description:
       "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
     date: "JULY 23, 2022",
-    image: "./src/assets/article-image-01.png",
+    image: "src/assets/article-image-01.png",
   },
 ];

d5d0d1b3c8e4a3f9cd20eba75092371c2f1f49be
pranav <pranavsagar530@gmail.com>
2026-04-01 11:13:36 +0530

    Refined File Paths

--- src/data.js
+++ src/data.js
@@ -1,23 +1,23 @@
 export const blogPosts = [
   {
-    title: "Blog One",
+    title: "Mastering CSS Grid",
     description:
-      "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-    date: "JULY 23, 2022",
-    image: "assets/article-image-03.png",
+      "Grid layout is a game-changer for responsive design. Today I learned how to create complex layouts with just a few lines of CSS using grid-template-areas.",
+    date: "OCTOBER 12, 2023",
+    image: "/assets/article-image-01.png",
   },
   {
-    title: "Blog Two",
+    title: "Clean Code: JavaScript Edition",
     description:
-      "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-    date: "JULY 23, 2022",
-    image: "assets/article-image-02.png",
+      "Writing code is easy, but writing readable code is an art. I've been exploring arrow functions, destructuring, and how to keep my functions small and focused.",
+    date: "NOVEMBER 05, 2023",
+    image: "/assets/article-image-02.png",
   },
   {
-    title: "Blog Three",
+    title: "Design for Developers",
     description:
-      "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-    date: "JULY 23, 2022",
-    image: "assets/article-image-01.png",
+      "I used to think design was just 'making things look pretty,' but it's really about user experience. Learning about white space and typography has changed my workflow.",
+    date: "DECEMBER 18, 2023",
+    image: "/assets/article-image-03.png",
   },
 ];
